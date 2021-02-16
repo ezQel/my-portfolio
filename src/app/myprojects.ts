5d736950c74cbbde58ce80abff51cf42ba8e3183
ezQel <ezekiel3499@gmail.com>
2021-02-17 02:54:51 +0300

    feat(projects): add myprojects
    an array of all my projects

---
+++ src/app/myprojects.ts
@@ -0,0 +1,26 @@
+export let MYPROJECTS = [
+    {
+      title: 'Mimea - plant Identification App',
+      description: 'An app that can identify a plant\'s species from a photo of the plant.',
+      sourceUrl: 'github.com/ezqel/mimea',
+      pageUrl: null,
+      imageUrl: '/assets/mimea.png',
+      techStack: ['python', 'android', 'tensorflow']
+    },
+    {
+      title: 'FullHouse - A Fictional Real Estate Website',
+      description: 'A real estate listings website.',
+      sourceUrl: 'github.com/ezqel/mimea',
+      pageUrl: null,
+      imageUrl: '/assets/fullhouse.png',
+      techStack: ['php', 'mysql', 'bootstrap']
+    },
+    {
+      title: 'Fechi - A fictional E-commerce site',
+      description: 'An e-commerce website',
+      sourceUrl: 'github.com/ezqel/mimea',
+      pageUrl: 'fechi-00.web.app',
+      imageUrl: '/assets/fechi.png',
+      techStack: ['angular', 'firebase']
+    },
+];

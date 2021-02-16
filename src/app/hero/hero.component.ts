d92171eed9829ae73b82f4db1435eb1af00f86a7
ezQel <ezekiel3499@gmail.com>
2021-02-17 02:52:16 +0300

    add contact icons

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
   paragraph = 'I am a software developer. Coding is my passion. I dream of writing code that will impact thousands of people positively someday. Check out some projects I\'ve built.';
-
+  paragraph0 = 'I am an IT graduate with a passion for software development. I\'m looking towards joining a team that together we can create a positive impact on thousands of people through code.';
+  paragraph1 = 'I am an IT graduate with a passion for software development. ';
   constructor() { }
 
   ngOnInit(): void {

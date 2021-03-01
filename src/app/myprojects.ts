1f69b030b8478207a1b9637dc3babf7a6a4bbccd
ezQel <ezekiel3499@gmail.com>
2021-03-01 17:44:31 +0300

    project model class

--- src/app/myprojects.ts
+++ src/app/myprojects.ts
@@ -1,26 +1,36 @@
-export let MYPROJECTS = [
+import { Project } from './project';
+
+export let MYPROJECTS: Project[] = [
     {
-      title: 'Mimea - plant Identification App',
-      description: 'An app that can identify a plant\'s species from a photo of the plant.',
-      sourceUrl: 'github.com/ezqel/mimea',
+      title: 'Mimea',
+      description: 'A plant Identification App',
+      sourceUrl: 'http://github.com/ezqel/mimea',
       pageUrl: null,
       imageUrl: '/assets/mimea.png',
       techStack: ['python', 'android', 'tensorflow']
     },
     {
-      title: 'FullHouse - A Fictional Real Estate Website',
+      title: 'FullHouse',
       description: 'A real estate listings website.',
-      sourceUrl: 'github.com/ezqel/mimea',
+      sourceUrl: 'http://github.com/ezqel/mimea',
       pageUrl: null,
       imageUrl: '/assets/fullhouse.png',
       techStack: ['php', 'mysql', 'bootstrap']
     },
     {
-      title: 'Fechi - A fictional E-commerce site',
+      title: 'Fechi',
       description: 'An e-commerce website',
-      sourceUrl: 'github.com/ezqel/mimea',
+      sourceUrl: 'http://github.com/ezqel/mimea',
       pageUrl: 'fechi-00.web.app',
       imageUrl: '/assets/fechi.png',
       techStack: ['angular', 'firebase']
+    },
+    {
+      title: 'Mimea',
+      description: 'A plant Identification App',
+      sourceUrl: 'http://github.com/ezqel/mimea',
+      pageUrl: null,
+      imageUrl: '/assets/mimea.png',
+      techStack: ['python', 'android', 'tensorflow']
     },
 ];
---
+++ src/app/project.ts
@@ -0,0 +1,8 @@
+export class Project {
+    title: string;
+    description: string;
+    sourceUrl: string;
+    pageUrl: string;
+    imageUrl: string;
+    techStack: string[];
+}
